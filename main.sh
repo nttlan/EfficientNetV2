8d05a4f0aeda0a030d7a843a4820b47919db553a
21521065 <96574488+nttlan@users.noreply.github.com>
2023-12-22 17:45:00 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,3 @@
 GPUS=$1
-python3 -m torch.distributed.launch --nproc_per_node=$GPUS main.py ${@:2}
+# python3 -m torch.distributed.launch --nproc_per_node=$GPUS main.py ${@:2}
+torchrun --nproc_per_node=$GPUS main.py ${@:2}
